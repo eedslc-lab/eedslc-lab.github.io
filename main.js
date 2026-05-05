05a6cc00350c259f03fe07ab81ab62cf519e305b
ElliotD <eedslc@gmail.com>
2026-05-05 21:25:45 +0200

    Initial Commit

---
+++ main.js
@@ -0,0 +1,8 @@
+let homeNav = document.getElementById("home-nav");
+let serviceNav = document.getElementById("service-nav");
+let aboutNav = document.getElementById("about-nav");
+let projectNav = document.getElementById("projects-nav");
+let contactNav = document.getElementById("contact-nav");
+homeNav.addEventListener("click" , () =>{
+    console.log("clicked")
+});
