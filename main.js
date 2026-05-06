99808098f3b22023fb5398317e84820fdac4c6ce
ElliotD <eedslc@gmail.com>
2026-05-06 20:19:11 +0200

    Adding to site

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ let serviceNav = document.getElementById("service-nav");
 let aboutNav = document.getElementById("about-nav");
 let projectNav = document.getElementById("projects-nav");
 let contactNav = document.getElementById("contact-nav");
+let headerTitle = document.getElementById("header-title");
+headerTitle.addEventListener("click", () => {
+    window.location.replace("index.html")
+})
 homeNav.addEventListener("click" , () =>{
     console.log("clicked")
 });
