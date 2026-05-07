6c650e43f02bf8764bac78f4e953b4fb5640a337
ElliotD <eedslc@gmail.com>
2026-05-08 00:55:28 +0200

    updated scrolling cards

--- main.js
+++ main.js
@@ -4,9 +4,20 @@ let aboutNav = document.getElementById("about-nav");
 let projectNav = document.getElementById("projects-nav");
 let contactNav = document.getElementById("contact-nav");
 let headerTitle = document.getElementById("header-title");
+let menuIndicate = document.getElementById("dropdown-indicator");
+let clicked =  "false";
+menuIndicate.addEventListener("click", () => {
+    if (clicked === "false") {
+        document.getElementById("navigation-menu").style.display = "flex";
+        clicked = "true";
+    } else {
+        document.getElementById("navigation-menu").style.display = "none";
+        clicked = "false";
+    };
+});
 headerTitle.addEventListener("click", () => {
     window.location.replace("index.html")
-})
+});
 homeNav.addEventListener("click" , () =>{
     console.log("clicked")
 });
